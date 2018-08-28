01903f13e535ea407a75cbd57302a1c54f9c00bd
YGDFLAZ <32245064+YGDFLAZ@users.noreply.github.com>
2018-08-28 18:57:00 +0200

    Built rough skeleton for code

---
+++ sensors.py
@@ -0,0 +1,11 @@
+def getTempreture(tempSensorPin):
+    #code to get getTempreture from sensor
+    #get info from pin: tempSensorPin
+    #turn it into usable data
+    return temp
+
+def getPot(potPin):
+    return pot
+
+def getLDR(ldrPin): #bla bla just examples of fetching data from sensors
+    return ldr
