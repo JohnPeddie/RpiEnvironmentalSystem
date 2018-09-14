fd5d5a1c677dcc9e5557af4447c2630fa67e6860
Max Chernotsky <max.chernotsky@me.com>
2018-09-14 14:40:23 +0200

    changes to main.py

--- main.py
+++ main.py
@@ -5,25 +5,27 @@ import sensors.py
 import display.py
 import datetime
 import time
+
 #global variables:
-tempSensorPin = 22 #examples, i dont know if these pins are correct
-potPin = 23
-ldrPin =24
-resetPin = 25
+#tempSensorPin = 22 #examples, i dont know if these pins are correct
+#potPin = 23
+#ldrPin =24
+resetPin = 23
 
 #ADC PINS
 SPICLK = 11
 SPIMISO = 9
-SPIMOSI = 10
+SPIMOSI = 10 
 SPICS = 8
-#bla bla more pins
-#might be better to make the pins an array/linkedList
 
 def main():
-    global tempSensorPin, potPin, ldrPin, resetPin, SPICLK, SPIMISO, SPIMOSI, SPICS
+    global resetPin, SPICLK, SPIMISO, SPIMOSI, SPICS
+    
     #run initialisation
     init.initADC(SPICLK, SPIMISO, SPIMOSI, SPICS)
     init.initPins(tempSensorPin,potPin,ldrPin,resetPin)
+    
+
     #bla bla just example code on how to call functions from another file
     timer = 0
     state = False #if state is false program is not running
@@ -38,11 +40,15 @@ def main():
             ## TODO: write function to choose between 500ms, 1s, 2s delay
             #if 500 ms chosen then let delay = 500ms etc etc
 
-            sysTime = datetime.datetime.now().time() #gets the current system time
-            display.display(sysTime, timer, sensors.getPot(potPin), sensors.getTempreture(tempSensorPin), sensors.getLDR(ldrPin))
-            timer += delay
-            time.sleep(delay)
-
+            #sysTime = datetime.datetime.now().time() #gets the current system time
+            #display.display(sysTime, timer, sensors.getPot(potPin), sensors.getTempreture(tempSensorPin), sensors.getLDR(ldrPin))
+            #timer += delay
+            #time.sleep(delay)
+            values = [0]*8
+            for i in range(8):
+                values[i] - mcp.read_adc(i)
+	    time.sleep(0.5)
+	    print values
 
 
 if __name__ == "__main__":
