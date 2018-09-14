b0970db211bb2f42afc1890aa7ea4d775b4b7386
jamesdevilliers <jamesrobertdevilliers@gmail.com>
2018-09-14 22:38:51 +0200

    working temp and potentiometer

--- sensors.py
+++ sensors.py
@@ -11,7 +11,9 @@ SPIMISO = 9
 SPIMOSI = 10
 SPICS = 8
 
-values = [0]*8
+pot_adc = 0
+temp_adc = 0
+light_adc = 0
 
 def getData(channel, delay):
         GPIO.setmode(GPIO.BCM)
@@ -21,12 +23,13 @@ def getData(channel, delay):
         GPIO.setup(SPICS, GPIO.OUT)
         mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
 
-	try:
-		while True:
-			values[0] = mcp.read_adc(0)
-			time.sleep(delay)
-			print values
-	except KeyboardInterrupt:
-			spi.close()
+	while True:
+		pot_adc = mcp.read_adc(0)
+		temp_adc = mcp.read_adc(1)
+		light_adc = mcp.read_adc(2)
+		potV = round((pot_adc/1024.0)*3.3, 2)
+		temp = round(((((temp_adc*3.3)/1024)-0.5)/0.01),2)
+		time.sleep(delay)
+		print temp
 
 	return values
