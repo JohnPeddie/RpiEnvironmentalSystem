474767823e6db2bcf7ef9ce22b2e1c1aa46c3f53
jamesdevilliers <jamesrobertdevilliers@gmail.com>
2018-09-14 22:46:03 +0200

    here

--- init.py
+++ init.py
@@ -6,7 +6,7 @@ import time
 def initPins(resetSwitch, frequencySwitch, stopSwitch, displaySwitch):
 	#initialise all the pins
 	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(resetPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) #eg of a pin being initialised
+	GPIO.setup(resetSwitch, GPIO.IN, pull_up_down=GPIO.PUD_UP) #eg of a pin being initialised
 	GPIO.setup(frequencySwitch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(stopSwitch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(displaySwitch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sensors
 import display
 import datetime
 import time
+import RPi.GPIO as GPIO
 
 #global variables:
 resetSwitch = 23
@@ -29,21 +30,12 @@ def main():
 	timer = 0
 	state = True #if state is false program is not running
     	while(True):
-		
-		
+
+
 
 		while(state):#will run until reset button is bushed
-			#if reset button pressed: state = False and break
 
-			## TODO: write function to choose between 500ms, 1s, 2s$
-			#if 500 ms chosen then let delay = 500ms etc etc
-			#sysTime = datetime.datetime.now().time() 
-			#gets the cur$
-			#display.display(sysTime, timer, sensors.getPot(potPin)$
-			#timer += delay
-			#time.sleep(delay)
-			
-			GPIO.add_event_detech(frequencySwitch, GPIO.FALLING, callback=frequencyChange, bouncetime=200)
+			GPIO.add_event_detect(frequencySwitch, GPIO.FALLING, callback=frequencyChange, bouncetime=200)
 
 
 			data = sensors.getData(0, delay)
@@ -51,12 +43,12 @@ def main():
 
 def frequencyChange (channel):
 	global delay
-	
-	if (delay = 0.5):
+
+	if (delay == 0.5):
 		delay = 1
-	elif (delay = 1):
+	elif (delay == 1):
 		delay = 2
-	elif (delay = 2):
+	elif (delay == 2):
 		delay = 0.5
 
 
