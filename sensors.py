612264616be9792e489a1f2e94649447d186fa64
Max Chernotsky <max.chernotsky@me.com>
2018-09-14 19:58:37 +0200

    fixed tabs

--- sensors.py
+++ sensors.py
@@ -7,21 +7,21 @@ import sys
 
 def getTemperature(channel):
 	adc = spi.xfer2([1,(8+channel)<<4,0]) # sending 3 bytes
-    	data = ((adc[1]&3) << 8) + adc[2]
-    	return data
+	data = ((adc[1]&3) << 8) + adc[2]
+	return data
 
 def ConvertVolts(data, places)
-    	volts = (data*3.3)/float(1023)
-    	volts = round(volts, places)
-    	return volts
+	volts = (data*3.3)/float(1023)
+	volts = round(volts, places)
+	return volts
 
 
 def checkSensor(channel, delay)
-    	spi = spidev.SpiDev()
-    	spi.open(0,0)
+	spi = spidev.SpiDev()
+	spi.open(0,0)
     
-    	try:
-        	while True:
+	try:
+		while True:
 			sensor_data = GetData(channel)
 			sensor_volt = ConvertVolts(sensor_data, 2)
 			time.sleep(delay)
