3418c2ff0b04fcb11a610905ff626a0c3728764a
Max Chernotsky <max.chernotsky@me.com>
2018-09-14 22:37:03 +0200

    enabled buttons and added code for frequency interrupt

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import datetime
 import time
 
 #global variables:
-resetPin = 23
+resetSwitch = 23
 frequencySwitch = 24
-stopSwitch = 22
-displaySwitch = 27
+stopSwitch = 27
+displaySwitch = 22
 
 #ADC PINS
 SPICLK = 11
@@ -17,19 +17,20 @@ SPIMISO = 9
 SPIMOSI = 10
 SPICS = 8
 
+#global variables
+delay = 0.5
 
 def main():
-	global resetPin, SPICLK, SPIMISO, SPIMOSI, SPICS
+	global resetSwitch, frequencySwitch, stopSwitch, displaySwitch, SPICLK, SPIMISO, SPIMOSI, SPICS
 
 	#run initialisation
-	init.initPins(resetPin)
+	init.initPins(resetSwitch, frequencySwitch, stopSwitch, displaySwitch)
 
 	timer = 0
 	state = True #if state is false program is not running
     	while(True):
-		#check if reset button is pushed to start program
-		#bla bla bla bla
-		#timer
+		
+		
 
 		while(state):#will run until reset button is bushed
 			#if reset button pressed: state = False and break
@@ -41,9 +42,23 @@ def main():
 			#display.display(sysTime, timer, sensors.getPot(potPin)$
 			#timer += delay
 			#time.sleep(delay)
+			
+			GPIO.add_event_detech(frequencySwitch, GPIO.FALLING, callback=frequencyChange, bouncetime=200)
 
-			data = sensors.getData(0, 0.5)
+
+			data = sensors.getData(0, delay)
 			print data
+
+def frequencyChange (channel):
+	global delay
+	
+	if (delay = 0.5):
+		delay = 1
+	elif (delay = 1):
+		delay = 2
+	elif (delay = 2):
+		delay = 0.5
+
 
 if __name__ == '__main__':
 	main()
