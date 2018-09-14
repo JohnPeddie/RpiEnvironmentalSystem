8e701dc77f82bf44837711070a181d39ab839ebd
Max Chernotsky <max.chernotsky@me.com>
2018-09-14 21:52:11 +0200

    testing

--- init.py
+++ init.py
@@ -3,26 +3,15 @@ import RPi.GPIO as GPIO
 import Adafruit_MCP3008 #required library for adc, see prac sheet for how to install adafruit
 import time
 
-global tempCLK
-global tempCS
-global tempMOSI
-global tempMISO
+global mcp
 
 def initADC(SPICLK, SPIMISO, SPIMOSI, SPICS):
-	global tempCLK, tempCS, tempMOSI, tempMISO
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(SPIMOSI, GPIO.OUT)
 	GPIO.setup(SPIMISO, GPIO.IN)
 	GPIO.setup(SPICLK, GPIO.OUT)
 	GPIO.setup(SPICS, GPIO.OUT)
-	tempCLK = SPICLK
-	tempCS = SPICS
-	tempMISO = SPIMISO
-	tempMOSI = SPIMOSI
-	
-
-
-#mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
+	mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
 
 
 def initPins(resetPin):
--- sensors.py
+++ sensors.py
@@ -3,15 +3,14 @@ import RPi.GPIO as GPIO
 import time
 import os
 import sys
-import globals
+import init
 
 values = [0]*8
 
 def getData(channel, delay):
-	mcp = Adafruit_MCP3008.MCP3008(clk=globals.tempCLK, cs=globals.tempCS, mosi=globals.tempMOSI, miso=globals.tempMISO)
 	try:
 		while True:
-			values[0] = mcp.read_adc(0)
+			values[0] = init.mcp.read_adc(0)
 			time.sleep(delay)
 	except KeyboardInterrupt:
 			spi.close()
