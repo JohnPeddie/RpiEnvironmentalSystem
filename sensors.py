833b1a033cc6d23f5a0f89131165a56475358bc2
jamesdevilliers <jamesrobertdevilliers@gmail.com>
2018-09-14 23:01:52 +0200

    final sensors all working

--- sensors.py
+++ sensors.py
@@ -14,8 +14,9 @@ SPICS = 8
 pot_adc = 0
 temp_adc = 0
 light_adc = 0
+values = [0]*3
 
-def getData(channel, delay):
+def getData():
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(SPIMOSI, GPIO.OUT)
         GPIO.setup(SPIMISO, GPIO.IN)
@@ -23,13 +24,14 @@ def getData(channel, delay):
         GPIO.setup(SPICS, GPIO.OUT)
         mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
 
-	while True:
-		pot_adc = mcp.read_adc(0)
-		temp_adc = mcp.read_adc(1)
-		light_adc = mcp.read_adc(2)
-		potV = round((pot_adc/1024.0)*3.3, 2)
-		temp = round(((((temp_adc*3.3)/1024)-0.5)/0.01),2)
-		time.sleep(delay)
-		print temp
+	pot_adc = mcp.read_adc(0)
+	temp_adc = mcp.read_adc(1)
+	light_adc = mcp.read_adc(2)
+	potV = round((pot_adc/1024.0)*3.3, 2)
+	temp = round(((((temp_adc*3.3)/1024)-0.5)/0.01),2)
+	light = round((light_adc/985.0)*100.0,2)
+	values[0] = potV
+	values[1] = temp
+	values[2] = light
 
 	return values
