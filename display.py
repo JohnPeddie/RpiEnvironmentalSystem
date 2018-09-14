4095d91678ab1d7d91dad013644adb7da8e62e75
YGDFLAZ <32245064+YGDFLAZ@users.noreply.github.com>
2018-09-14 16:54:30 +0200

    updated display.py to work. Ready to merge to master

--- display.py
+++ display.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python
+readings = [("Time", "Timer", "Pot", "Temp", "Light")]
 def display(sysTime, timer, Pot, Temp, Light):
-    if (timer == 0): #checks for first time it is being called inorder to print the heading line:
-        print "Time Timer Pot Temp Light"
+    global readings
+    readings.append((sysTime, timer, Pot, Temp, Light))
 
-        #construct data table
-    #bla bla bla bla needs to be a nice table
+
+    for reading in readings:
+        print '\t'.join(str(i) for i in reading)
+        print("-------------------------------------")
+
+
+
+
+
+
+# def format_matrix(header, matrix,
+#                   top_format, left_format, cell_format, row_delim, col_delim):
+#     table = [[''] + header] + [[name] + row for name, row in zip(header, matrix)]
+#     table_format = [['{:^{}}'] + len(header) * [top_format]] \
+#                  + len(matrix) * [[left_format] + len(header) * [cell_format]]
+#     col_widths = [max(
+#                       len(format.format(cell, 0))
+#                       for format, cell in zip(col_format, col))
+#                   for col_format, col in zip(zip(*table_format), zip(*table))]
+#     return row_delim.join(
+#                col_delim.join(
+#                    format.format(cell, width)
+#                    for format, cell, width in zip(row_format, row, col_widths))
+#                for row_format, row in zip(table_format, table))
+
