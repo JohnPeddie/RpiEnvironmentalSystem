ac876eaf561db8f892c43782866cfcfb420ddf44
Max Chernotsky <max.chernotsky@me.com>
2018-09-14 21:10:06 +0200

    only using AdaFruit library

--- sensors.py
+++ sensors.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-import spidev
 import time
 import os
 import sys
-spi = spidev.SpiDev()
 
-def getData(channel):
-	adc = spi.xfer2([1,(8+channel)<<4,0]) # sending 3 bytes
-	data = ((adc[1]&3) << 8) + adc[2]
-	return data
+values = [0]*8
 
-def ConvertVolts(data, places):
-	volts = (data*3.3)/float(1023)
-	volts = round(volts, places)
-	return volts
-
-
-def getSensors(channel, delay):
-	global spi
-	spi.open(0,0)
-
+def getData(channel, delay)
 	try:
 		while True:
-			sensor_data = getData(channel)
-			sensor_volt = ConvertVolts(sensor_data, 2)
+			for i in range(8):
+				values[i] = mcp.read_adc(i)
 			time.sleep(delay)
-			print sensor_volt
 	except KeyboardInterrupt:
 			spi.close()
 
-	return sensor_volt
+	return values
