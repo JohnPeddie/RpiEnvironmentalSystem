c392e7e657eb7851df87cac5e63e577c0a6b40f6
YGDFLAZ <32245064+YGDFLAZ@users.noreply.github.com>
2018-09-14 15:21:17 +0200

    Merged from init

--- init.py
+++ init.py
@@ -2,9 +2,15 @@
 import RPi.GPIO as GPIO
 import Adafruit_MCP3008 #required library for adc, see prac sheet for how to install adafruit
 
-def initADC():
-    # TODO: set up adc number initialisation that can be set in main.py
-    #initialise ADC
+def initADC(SPICLK, SPIMISO, SPIMOSI, SPICS):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SPIMOSI, GPIO.OUT)
+    GPIO.setup(SPIMISO, GPIO.IN)
+    GPIO.setup(SPICLK, GPIO.OUT)
+    GPIO.setup(SPICS, GPIO.OUT)
+    mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
+    #Add code to deal with mcp
+
 
 def initPins(tempSensorPin,potPin,ldrPin,resetPin):
     #initialise all the pins
