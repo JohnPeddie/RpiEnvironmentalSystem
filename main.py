990cdd51a164671e62650329002a3982dd5616d8
YGDFLAZ <32245064+YGDFLAZ@users.noreply.github.com>
2018-09-16 21:22:36 +0200

    changed append

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
 			end_time = time.time()
 			timer = round(end_time-start_time,0)
 			data[1] = timer
-			displayData.append(data)
+			displayData+= [data]
 			print (displayData)
 
 def frequencyChange (channel):
